2feb296d56ec6e35056aab4b566f01f42567cb95
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-12-07 13:25:05 -0500

    [FE-1235] corrected the imports for ActiveFilters

--- src/components/reportBuilder/index.js
+++ src/components/reportBuilder/index.js
@@ -1,2 +1,3 @@
 export { default as ReportsListModal } from './ReportsListModal';
 export { default as ChangeReportModal } from './ChangeReportModal';
+export { default as ActiveFilters } from './ActiveFilters';
