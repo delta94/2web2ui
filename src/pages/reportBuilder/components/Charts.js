f10cef82c01cd6ff7aa268b6a1f7c3c4b02912d1
Jon Ambas <jon@jonambas.com>
2020-12-07 12:34:32 -0500

    FE-1273 Abstract report builder LineChart

--- src/pages/reportBuilder/components/Charts.js
+++ src/pages/reportBuilder/components/Charts.js
@@ -1,7 +1,7 @@
 import React, { useMemo, useState } from 'react';
 import _ from 'lodash';
 import { getLineChartFormatters } from 'src/helpers/chart';
-import LineChart from './LineChart';
+import LineChart from 'src/components/charts/LineChart';
 import METRICS_UNIT_CONFIG from 'src/config/metrics-units';
 import { Box, Stack, Panel } from 'src/components/matchbox';
 import { useSparkPostQuery } from 'src/hooks';
@@ -16,6 +16,8 @@ import { useReportBuilderContext } from '../context/ReportBuilderContext';
 import { ApiErrorBanner } from 'src/components';
 import Loading from 'src/components/loading/PanelLoading';
 import { Heading } from 'src/components/text';
+import { Loading } from 'src/components';
+import CustomTooltip from './Tooltip';
 const DEFAULT_UNIT = 'number';
 
 function getUniqueUnits(metrics) {
@@ -163,6 +165,7 @@ export function Charts(props) {
               yLabel={chart.label}
               tooltipValueFormatter={chart.yAxisFormatter}
               showXAxis={index === charts.length - 1}
+              tooltip={CustomTooltip}
             />
           </Box>
         ))}
