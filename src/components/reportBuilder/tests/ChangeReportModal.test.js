0a847a764d7f3ecb836eb3368bbe0f9508b6db29
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-12-07 15:39:51 -0500

    [FE-1192] updated unit tests

--- src/components/reportBuilder/tests/ChangeReportModal.test.js
+++ src/components/reportBuilder/tests/ChangeReportModal.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { ReportsListModal } from '../ReportsListModal';
+import { ChangeReportModal } from '../ChangeReportModal';
 import TestApp from 'src/__testHelpers__/TestApp';
 
-describe('ReportsListModal', () => {
+describe('ChangeReportModal - on Dashboard', () => {
   const defaults = {
     currentUser: 'Sparky McSparkFace',
     handleDelete: jest.fn(),
     handleEdit: jest.fn(),
     open: true,
+    onDashboard: true,
     reports: [
       {
         id: 0,
@@ -30,7 +31,7 @@ describe('ReportsListModal', () => {
   const subject = props => {
     render(
       <TestApp isHibanaEnabled={true}>
-        <ReportsListModal {...defaults} {...props} />
+        <ChangeReportModal {...defaults} {...props} />
       </TestApp>,
     );
   };
@@ -52,40 +53,22 @@ describe('ReportsListModal', () => {
     expect(screen.getByText('All Reports')).toBeVisible();
     testFirstTab(['Name', 'Last Modification', 'My Saved Report', /Sep [123] 2020/]);
     expect(screen.queryByText('Someone Elses Report')).not.toBeInTheDocument();
+    expect(screen.getAllByRole('radio')).toHaveLength(1);
 
     screen.getByText('All Reports').click();
+    expect(screen.getAllByRole('radio')).toHaveLength(2);
     testSecondTab(['Name', 'Last Modification', 'My Saved Report', /Sep [123] 2020/]);
     expect(screen.getByText('Created By')).toBeVisible();
     expect(screen.getByText('Sparky McSparkFace')).toBeVisible();
 
-    expect(screen.queryByRole('button', { name: 'Change Report' })).not.toBeInTheDocument();
-    expect(screen.queryByRole('button', { name: 'Cancel' })).not.toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Change Report' })).toBeVisible();
+    expect(screen.getByRole('button', { name: 'Cancel' })).toBeVisible();
   });
 
-  it('does not render action list for not editable reports', () => {
+  it('does not render action list for reports', () => {
     subject();
     screen.getByText('All Reports').click();
-    expect(screen.queryByTestId('popover-allreports-0')).toBeInTheDocument();
+    expect(screen.queryByTestId('popover-allreports-0')).not.toBeInTheDocument();
     expect(screen.queryByTestId('popover-allreports-1')).not.toBeInTheDocument();
-  });
-
-  it('handles delete when clicking delete in the actionlist', () => {
-    subject();
-    screen.getByText('All Reports').click();
-    screen.getByTestId('popover-allreports-0').click();
-    screen.getByText('Delete').click();
-    expect(defaults.handleDelete).toHaveBeenCalledWith(
-      expect.objectContaining({ id: 0, name: 'My Saved Report' }),
-    );
-  });
-
-  it('handles edit when clicking edit in the actionlist', () => {
-    subject();
-    screen.getByText('All Reports').click();
-    screen.getByTestId('popover-allreports-0').click();
-    screen.getByText('Edit').click();
-    expect(defaults.handleEdit).toHaveBeenCalledWith(
-      expect.objectContaining({ id: 0, name: 'My Saved Report' }),
-    );
   });
 });
