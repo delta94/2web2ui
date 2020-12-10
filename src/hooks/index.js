bbbbeb619c44e0c00b839f5147e6760ffef23d0d
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-12-10 15:45:23 -0500

    [FE-1246] Added useEmptyState hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -11,3 +11,4 @@ export { default as useProvidedContext } from './useProvidedContext';
 export { default as useSparkPostQuery } from './useSparkPostQuery';
 export { default as useTabs } from './useTabs';
 export { default as useUniqueId } from './useUniqueId';
+export { default as useEmptyState } from './useEmptyState';
