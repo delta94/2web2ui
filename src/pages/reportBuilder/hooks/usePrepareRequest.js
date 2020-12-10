569bdecfdd4e5eaba91842c0309dbc21d316ef26
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-12-10 16:28:59 -0500

    SA-1849 - Use usePrepareQuery hook for CompareByAggregatedMetrics component

--- src/pages/reportBuilder/hooks/usePrepareRequest.js
+++ src/pages/reportBuilder/hooks/usePrepareRequest.js
@@ -3,13 +3,11 @@ import {
   getMetricsFromKeys,
   getQueryFromOptionsV2 as getQueryFromOptions,
 } from 'src/helpers/metrics';
-import { useReportBuilderContext } from '../context/ReportBuilderContext';
 
 /**
  * Prepares options for requests based on the current state of the page
  */
-export default function usePrepareComparisonRequest() {
-  const { state: reportOptions } = useReportBuilderContext();
+export default function usePrepareReportBuilderQuery(reportOptions) {
   const { metrics } = reportOptions;
   const formattedMetrics = useMemo(() => {
     return getMetricsFromKeys(metrics, true);
