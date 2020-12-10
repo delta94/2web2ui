f9539e46e6c77fff50be54c0631674c0701f3c03
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-12-10 12:06:09 -0500

    [FE-1235-2] Added cypress test for the Modal

--- src/pages/dashboardV2/components/PinnedReportFiltersModal.js
+++ src/pages/dashboardV2/components/PinnedReportFiltersModal.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Modal, Button } from 'src/components/matchbox';
 import { ActiveFilters } from 'src/components/reportBuilder';
 
 export default function PinnedReportFiltersModal({ pinnedReport, open, onClose }) {
+  const history = useHistory();
   return (
     <Modal open={open} onClose={onClose} showCloseButton maxWidth="1300">
       <Modal.Header>{pinnedReport.name} Filters </Modal.Header>
@@ -10,7 +12,7 @@ export default function PinnedReportFiltersModal({ pinnedReport, open, onClose }
         <ActiveFilters filters={pinnedReport.options.filters} />
       </Modal.Content>
       <Modal.Footer>
-        <Button variant="primary" to={pinnedReport.linkToReportBuilder}>
+        <Button variant="primary" onClick={() => history.push(pinnedReport.linkToReportBuilder)}>
           View Report
         </Button>
         <Button variant="secondary" onClick={onClose}>
