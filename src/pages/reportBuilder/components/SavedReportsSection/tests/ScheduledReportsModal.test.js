b5276c4fa926e128d0c8dcc9393552a1d1297953
Kevin Chu <kevinchu1337@gmail.com>
2020-12-10 10:45:07 -0500

    SA-1756: Add Deletion of Scheduled Reports (#1928)
    * SA-1756: Add delete section to scheduled reports edit page
    
    * SA-1756: Added delete action to saved reports modal
    
    * SA-1756: Address Feedback
    
    * SA-1756: Fix bug and add regression test
    
    * SA-1756: Address final feedback

--- src/pages/reportBuilder/components/SavedReportsSection/tests/ScheduledReportsModal.test.js
+++ src/pages/reportBuilder/components/SavedReportsSection/tests/ScheduledReportsModal.test.js
@@ -1,14 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import ScheduledReportsModal from '../ScheduledReportsModal';
 import TestApp from 'src/__testHelpers__/TestApp';
-import { getScheduledReports } from 'src/actions/reports';
+import { deleteScheduledReport, getScheduledReports } from 'src/actions/reports';
 jest.mock('src/actions/reports');
 const defaultStore = {
   reports: {
+    deleteSchedulePending: false,
     getScheduledReportsStatus: 'idle',
     scheduledReports: [
       {
+        report_id: '123',
         schedule_id: '12345',
         name: 'My Scheduled Report',
         recipients: ['sparky@sparkpots.com', 'marketing@sparkpost.com'],
@@ -18,7 +20,8 @@ const defaultStore = {
 };
 
 describe('Scheduled Reports Modal', () => {
-  getScheduledReports.mockImplementation(() => jest.fn());
+  getScheduledReports.mockImplementation(() => jest.fn(() => Promise.resolve()));
+  deleteScheduledReport.mockImplementation(() => jest.fn(() => Promise.resolve()));
   const mockClose = jest.fn();
   const subject = (props, store = defaultStore) => {
     const defaultProps = {
@@ -70,5 +73,16 @@ describe('Scheduled Reports Modal', () => {
   it('closes the modal if an error has occurred', () => {
     subject({}, { reports: { getScheduledReportsStatus: 'error' } });
     expect(mockClose).toHaveBeenCalled();
+  });
+
+  it('handles delete properly', () => {
+    subject();
+    fireEvent.click(screen.getByRole('button', { name: 'Open Menu' }));
+    fireEvent.click(screen.getByRole('button', { name: 'Delete' }));
+    expect(
+      screen.queryByText('Are you sure you want to delete this scheduled report?'),
+    ).toBeVisible();
+    fireEvent.click(screen.getByRole('button', { name: 'Delete' }));
+    expect(deleteScheduledReport).toHaveBeenCalledWith('123', '12345');
   });
 });
