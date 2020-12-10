64a76a4a40aa023c6c36f6088e3cb2803472ed5c
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-12-10 18:40:25 -0500

    [FE-1246] Added loading state to container

--- src/pages/abTesting/ListPage.container.js
+++ src/pages/abTesting/ListPage.container.js
@@ -4,17 +4,22 @@ import _ from 'lodash';
 import { listAbTests, deleteAbTest, cancelAbTest } from 'src/actions/abTesting';
 import { showAlert } from 'src/actions/globalAlert';
 import { ListPage } from './ListPage';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useEmptyState } from 'src/hooks';
 import AbTestEmptyState from './components/EmptyState';
+import { Loading } from 'src/components';
 
 function ListPageContainer(props) {
-  const { abTests, listAbTests } = props;
+  const { abTests, listAbTests, loading } = props;
   useEffect(() => {
     listAbTests();
   }, [listAbTests]);
 
   const component = useEmptyState(abTests.length === 0, ListPage, AbTestEmptyState)(props);
+
+  if (loading) {
+    return <Loading />;
+  }
 
   return component;
 }
