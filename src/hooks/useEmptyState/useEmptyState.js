c68e4eec5aa95c6395f7deb3df9d88c817bfa52c
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-12-11 11:42:32 -0500

    [FE-1246] Added segment tracking

--- src/hooks/useEmptyState/useEmptyState.js
+++ src/hooks/useEmptyState/useEmptyState.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { useLocation } from 'react-redux';
+import { segmentTrack, SEGMENT_EVENTS } from 'src/helpers/segment';
 
 export default function useEmptyState(isEmpty, Page, EmptyState) {
+  const location = useLocation();
+  if (isEmpty) {
+    segmentTrack(SEGMENT_EVENTS.EMPTY_STATE_LOADED, {
+      location: location,
+    });
+  }
   return props => (isEmpty ? <EmptyState /> : <Page {...props} />);
 }
